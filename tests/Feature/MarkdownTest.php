5944045eaef64b32ad34d463956757b448254edb
emmanuel abraham <sundayemmanuelabraham@gmail.com>
2020-03-10 01:00:56 +0100

    setup phpunit suite

---
+++ tests/Feature/MarkdownTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace emmy\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class MarkdownTest extends TestCase
+{
+    /** @test */
+    public function experiment()
+    {
+
+    }
+}
